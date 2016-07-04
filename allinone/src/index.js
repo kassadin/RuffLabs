20f5c0905468f28b10bb5f73b0e96445a68ad2bd
kassadin <kassadin@foxmail.com>
2016-07-04 22:42:56 +0800

    add led and buzzer

--- allinone/src/index.js
+++ allinone/src/index.js
@@ -40,10 +40,12 @@ $.ready(function (error) {
         dht.getRelativeHumidity(function (error, humidity) {
             printToLcd('HUMI', error, humidity, 1);
         });
+        buzzer.turnOn();
     });
 
     btnr.on('release', function () {
         console.log('Button red released.');
+        buzzer.turnOff();
     });
     
     // btn-b 
@@ -68,11 +70,13 @@ $.ready(function (error) {
                 lcd._p3.write(0);
                 lcd.turnOff();
                 lcdStatus = 0;
+                closeLed();
             }else{
                 lcd._p3.write(1);
                 lcd.turnOn();
                 lcdStatus = 1;
                 welcome();
+               
             }
         }
     });
@@ -89,6 +93,7 @@ function welcome(){
     lcd.print('Hello,Ruff!');
     lcd.setCursor(0,1);
     lcd.print('kassadin.vip');
+     openLed();
 }
 
 function printToLcd(label,error,value,line){
@@ -103,4 +108,37 @@ function printToLcd(label,error,value,line){
     }
 
     lcd.print(content); 
+}
+
+// https://github.com/RuffApps/Apps/blob/master/colorful-light/src/index.js
+var lightTimer;
+function openLed(){
+    led.turnOn();
+    var r, g, b;
+    lightTimer =  setInterval(function () {
+        //产生0-255的随机颜色值
+        r = Math.random() * 255;
+        g = Math.random() * 255;
+        b = Math.random() * 255;
+
+        led.setRGB(r, g, b);
+        sleep(1000);
+    }, 2000);
+}
+
+//睡眠函数
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
+function closeLed(){
+    if(lightTimer){
+        clearInterval(lightTimer);
+    }
+    led.turnOff();
 }
